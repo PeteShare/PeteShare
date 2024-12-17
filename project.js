95755b0d91b1f0cb243ac210385a02b764cda71c
PeteShare <lampelapeetu@gmail.com>
2024-12-17 19:57:46 +0200

    Update project.js

--- project.js
+++ project.js
@@ -1,1 +1,19 @@
-const hello
+// adblocker for youtube
+let adBlocker = true;
+  const advertisement = document.getElementById('advertisement');
+    function adBlocke() {
+      if (adBlocker === true) {
+        advertisement.style.display = "none";
+      }
+  }
+if (adBlocke === true) {
+  window.reload();
+} else {
+  console.log("adblocke didnt work.")
+}
+  switch (advertisement) {
+    case 1:
+      advertisement = advertisement
+    break;
+  }
+adBlocke();
